70afac6d5ff02200d6e86aa4cea6253e74f0f595
Mathieu STUMPF-GUNTZ <mathieu@culture-libre.org>
2026-08-12 15:21:16 +0200

    Let’s use "standard·input·source·identity" as key

--- locale/esperanto/esperanto.lua
+++ locale/esperanto/esperanto.lua
@@ -150,7 +150,7 @@ locale = {
     ["incomplete·input·marker"] = "<dosierfino>",
     ["interactive·source·identity"] = "=ĉefenigujo",
     ["commandline·source·identity"] = "=(komandlinio)",
-    ["stdin·source·identity"] = "=normenigo",
+    ["standard·input·source·identity"] = "=normenigo",
   },
 
   diagnostics = {
--- lua.c
+++ lua.c
@@ -812,7 +812,7 @@ static int multiline (lua_State *L) {
     int exprstatus = LUA_OK;
     if (!stmt_incomplete && status != LUA_OK) {
       retline = lua_pushfstring(L, "%s %s", rtkw, line);
-      { const char *stdin_id = locale_get(L, "repl", "stdin·source·identity", "=stdin");
+      { const char *stdin_id = locale_get(L, "repl", "standard·input·source·identity", "=stdin");
         exprstatus = luaL_loadbufferx(L, retline, strlen(retline), stdin_id, "t"); }
       expr_incomplete = incomplete(L, exprstatus);
       if (exprstatus == LUA_OK) {
@@ -829,7 +829,7 @@ static int multiline (lua_State *L) {
       if (expr_incomplete && !stmt_incomplete && status != LUA_OK) {
         lua_pop(L, 1);  /* remove statement error */
         retline = lua_pushfstring(L, "%s %s;", rtkw, line);
-        { const char *stdin_id = locale_get(L, "repl", "stdin·source·identity", "=stdin");
+        { const char *stdin_id = locale_get(L, "repl", "standard·input·source·identity", "=stdin");
           exprstatus = luaL_loadbufferx(L, retline, strlen(retline), stdin_id, "t"); }
         lua_remove(L, -2);  /* remove generated "return" line */
         return exprstatus;  /* report expression-side incomplete error */
